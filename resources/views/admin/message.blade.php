062a95b3aa6e24bb549fdf7b7d32c375a6429dfa
prem-143 <57987335+prem-143@users.noreply.github.com>
2024-10-03 12:21:34 +1000

    first commit

---
+++ resources/views/admin/message.blade.php
@@ -0,0 +1,13 @@
+@if(Session::has('error'))
+<div class="alert alert-danger alert-dismissible">
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+    <h4><i class="icon fa fa-ban"></i> Alert!</h4>{{Session::get('error')}}
+
+</div>
+@endif
+@if(Session::has('success'))
+<div class="callout callout-success">
+    <h4>I</h4>
+    <p></p>{{Session::get('success')}}
+</div>
+@endif
