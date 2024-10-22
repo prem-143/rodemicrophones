af61c289f4bcbb4d794b1195137843baeefe01cd
prem-143 <57987335+prem-143@users.noreply.github.com>
2024-10-22 23:26:47 +1100

    second commit

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\admin\CategoryController;
 use App\Http\Controllers\admin\HomeController;
+
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\admin\AdminLoginController;
+
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,5 +35,28 @@ Route::group(['prefix'=>'admin'],function(){
     Route::group(['middleware'=>'admin.auth'],function (){
         Route::get('/dashboard',[HomeController::class,'index'])->name('admin.dashboard');
         Route::get('/logout',[HomeController::class,'logout'])->name('admin.logout');
+        //category routes
+        Route::any('/categories/create',[CategoryController::class,'create'])->name('categories.create');
+
+        Route::any('/categories',[CategoryController::class,'store'])->name('categories.store');
+
+
+
+        Route::get('/getSlug',function (Request $request)
+        {
+
+                 $slug = '';
+
+                if(!empty($request->title))
+                {
+                    $slug = Str::slug($request->title);
+
+                }
+                return response()->json([
+
+                    'slug' => $slug,
+                    'status' =>true
+                ]);
+        })->name('getSlug');
     });
 });
